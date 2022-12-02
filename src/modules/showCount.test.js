61d4d182a19906794ffdb3985c5a7c2233b4ab77
Aster Alemu <leaster2014w@gmail.com>
2022-12-02 11:29:13 -0800

    Add test

--- src/modules/showCount.test.js
+++ src/modules/showCount.test.js
@@ -17,13 +17,10 @@ describe('Test To Count Number of Items in  A Dom Element', () => {
   test('countItems', () => {
     document.body.innerHTML = `
     <ul class id="item-container">
-      <li>One item</li>
-      <li>Two item</li>
-
-    </ul>
+         </ul>
     `;
 
     const itemContainer = document.getElementById('item-container');
-    expect(countItems(itemContainer)).toBe(2);
+    expect(countItems(itemContainer)).toBe(0);
   });
 });
