adcf3dd99015d450a9f0fde7ba3c20e19221f425
MosDevx <mosesk.wamae@gmail.com>
2022-12-02 20:38:11 +0300

    slight changes

--- src/modules/involvment.js
+++ src/modules/involvment.js
@@ -1,54 +1,39 @@
-// import {involve_url,api_id} from './data.js'
+// import {involveUrl,apiId} from './data.js'
 // require('dotenv').config()
 
-const api_id = 'j26xtAx7qVMwcqU9i8K3';
-const involve_url = "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/";
-const tvmaze_url = "https://api.tvmaze.com/schedule/web?date=2022-11-30&country=US";
+const apiId = 'j26xtAx7qVMwcqU9i8K3';
+const involveUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
 
-const base_url = involve_url+api_id
+const baseUrl = involveUrl + apiId;
 
-// console.log("involve_url")
+// console.log("involveUrl")
 
-async function getLikes(){
+const likesEndpoint = `${baseUrl}/likes/`;
 
-    // console.log('get likes')
-    let response = await fetch(base_url)
-    let data = await response.JSON.parse()
-    return data
+async function postLikes(showIdArray) {
+  const likesArray = [];
+  showIdArray.forEach((id) => {
+    const like = {
+      likes: 0,
+      item_id: id,
+    };
+    likesArray.push(like);
+  });
+
+  const response = await fetch(likesEndpoint, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+
+    body: JSON.stringify(likesArray),
+
+  });
+
+  await response.json();
 }
 
-let likesEndpoint = base_url + '/likes/'
-
-console.log(likesEndpoint)
-async function postLikes(showIdArray){
-    
-
-    let likesArray = []
-    showIdArray.forEach((id)=>{
-        let like = {
-            "likes":0,
-            "item_id":id
-        }
-        likesArray.push(like)
-    })
-
-    let response = await fetch(likesEndpoint,{
-        method:'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-
-          body: JSON.stringify(likesArray)
-
-    });
-
-    const reply = await response.json()
-    console.log(reply)
-    
-
-}
-
-postLikes(['101','102','103','104'])
+postLikes(['101', '102', '103', '104']);
 
 // export default getLikes
---
+++ src/modules/likesApi.js
@@ -0,0 +1,22 @@
+const involveUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/j26xtAx7qVMwcqU9i8K3/likes/';
+
+async function postLike(showId) {
+  const response = await fetch(involveUrl, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ item_id: showId }),
+  });
+
+  await response.json();
+}
+
+async function getLikes() {
+  const response = await fetch(involveUrl);
+
+  return response.json();
+}
+
+export { postLike, getLikes };
